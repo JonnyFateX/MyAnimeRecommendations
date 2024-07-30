12b55d77fdc6e915b26cc392c1c5411ecd20ee56
Jonathan Martinez <jonathanmtzcon@gmail.com>
2024-07-30 15:18:22 -0600

    Fixed header logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <header>
-            <img  className="web-icon" src="../src/assets/icons/cloud.png" alt="cloud icon" />
+            <img className="web-icon" src="./assets/icons/cloud.png" alt="cloud icon" />
             <h1>MyAnimeRecommendations</h1>
         </header>
     )
