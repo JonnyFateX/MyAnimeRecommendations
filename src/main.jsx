11228d38b39e8e98fc0b255d94706cf393d8796e
Jonathan Martinez <jonathanmtzcon@gmail.com>
2024-07-31 16:23:55 -0600

    Fixed screen orientation

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-screen.orientation.lock("landscape")
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
