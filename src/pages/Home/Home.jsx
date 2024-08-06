05cc4e64edbd57622353a02a30b2105f911ca938
Jonathan Martinez <jonathanmtzcon@gmail.com>
2024-08-06 13:16:49 -0600

    Added background to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,19 @@ import './Home.css'
 
 export default function Home({onClick}){
     return (
+      <>
+        <div className='background-image'></div>
         <div className='home-container'>
-          <div className='home--title'>
-            <h1>MAR</h1>
+          <div className='home--content'>
+            <div className='home--title'>
+              <h1>MAR</h1>
+            </div>
+            <p>
+              Get anime recommendations from MyAnimeList. 
+            </p>
+            <button className='homeBtn' onClick={onClick}>Get Started</button>
           </div>
-          <p>
-            Get anime recommendations from MyAnimeList. 
-          </p>
-          <button className='homeBtn' onClick={onClick}>Get Started</button>
-          
         </div>
+      </>
       )
 }
